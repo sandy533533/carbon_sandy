5dbbbb935f88f165d08c1d69c25195c48558b71b
“sandy533533” <sandy_6536@163.com>
2021-10-21 17:32:05 +0800

    has mode

--- has_mode/mod_ing.h
+++ has_mode/mod_ing.h
@@ -1,60 +1,48 @@
-////////////////////////////////////////////////////////
-// Project： SystemC carbon
-// Module:   ing.h
-// Description: Pkt to cell, look up table then enter the queue (Pkt转Cell，查表入队列)
-// Group：
-// Author: Sandy
-// Date: 2021.10.11  new
-// Hierarchy : --top
-//             --|--ing.h
-//    pktgen------>|ing-->sch-->pe-->eng|----->stat
-////////////////////////////////////////////////////////
-
 #ifndef __MOD_ING_H__
 #define __MOD_ING_H__
-
 #include "comm_def.h"
 #include "comm_func.h"
 
-struct mod_ing : sc_module {
+////////////////////////////////////////////////////////
+// Project： SystemC虚拟项目
+// Module:   mod_ing
+// Description: mod_ing 入口ing模块h文件
+// Group：预研组
+// Author: Newton
+// Date: 2021.10.14 第一版
+// Hierarchy : 编号，索引公共库
+////////////////////////////////////////////////////////
+class mod_ing : public sc_module
+{
+public:
+    sc_in<int> clkcnt;
+    array<sc_in<s_pkt_desc> *, g_inter_num> in_port;
+    // output
+    sc_out<s_pkt_desc> out_cell_que;
 
-    public:
+public:
+    mod_ing(sc_module_name mod_ing);
+    SC_HAS_PROCESS(mod_ing);
+    void main_process();
+    void rev_pkt_process();
+    void port_rr_sch_process();
+    void lut_process();
+    void pkt_to_cell_process();
 
-        sc_in          <int>                     clkcnt; 
-        array <sc_in<s_pkt_desc>*,g_inter_num>   in_port;
- //output
-        sc_out         <s_pkt_desc>              out_cell_que;     
-        
-    public:    
+private:
+    s_pkt_desc s_port_sch_result;
+    int que_id;
+    int flow_id;
+    int pkt_tmp_len = 0;
+    int pkt_out_flag = 0;
+    s_flow_rule flow_rule;
 
-        mod_ing(sc_module_name name);
-        SC_HAS_PROCESS(mod_ing);
-   
-        void                                     main_process();
-        void                                     rev_pkt_process();
-        void                                     port_rr_sch_process();
-        void                                     lut_process();
-        void                                     pkt_to_cell_process();
-        RR_SCH                                   *rr_sch;
+    array<deque<s_pkt_desc>, g_inter_num>
+        fifo_port;
+    array<int, g_inter_num> pkt_count_port;
+    array<int, g_inter_num> infifo_count_port;
+    array<int, g_inter_num> drop_count_port;
 
-    private :    
-
-        s_pkt_desc                               s_port_sch_result;
-        int                                      que_id   ;
-        int                                      flow_id   ;
-        int                                      pkt_tmp_len = 0 ;
-        int                                      pkt_out_flag = 0 ;
-        s_flow_rule                              flow_rule;
-
-       
- //       array <fifo,g_inter_num>                 fifo_port;
-        array <deque<s_pkt_desc>,g_inter_num>    fifo_port;
-
-        array <int,g_inter_num>                  pkt_count_port; 
-        array <int,g_inter_num>                  infifo_count_port; 
-        array <int,g_inter_num>                  drop_count_port;
-   
-
+    RR_SCH *rr_sch;
 };
-   
 #endif
